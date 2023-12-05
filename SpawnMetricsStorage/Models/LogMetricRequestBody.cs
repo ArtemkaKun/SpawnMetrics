efad31e2bfc1737ed17c239f61a7beff00139973
Artem Yurchenko <zumikq78@gmail.com>
2023-12-05 22:54:24 +0100

    Refactor: add note comment to LogMetricRequestBody

--- SpawnMetricsStorage/Models/LogMetricRequestBody.cs
+++ SpawnMetricsStorage/Models/LogMetricRequestBody.cs
@@ -3,6 +3,8 @@ using SpawnMetricsStorage.Models.MetricRecordFiles;
 
 namespace SpawnMetricsStorage.Models;
 
+// NOTE: This class can be readonly structure instead of class, which will be more convenient dues to the logic.
+// Unfortunately, MiniValidation lib, that used to validate this structure, doesn't support validation of value types.
 public sealed class LogMetricRequestBody
 {
     [Required]
