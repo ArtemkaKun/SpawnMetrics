bce5719571d850db22b2c1141dafba0ed0a96e59
Artem Yurchenko <zumikq78@gmail.com>
2023-12-05 20:16:06 +0100

    Refactor: MetricRecord become a readonly struct with readonly structures

--- SpawnMetricsStorage/Models/MetricRecordFiles/MetricRecord.cs
+++ SpawnMetricsStorage/Models/MetricRecordFiles/MetricRecord.cs
@@ -1,36 +1,40 @@
 using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
 
 namespace SpawnMetricsStorage.Models.MetricRecordFiles;
 
-public sealed class MetricRecord
+// NOTE: This structure can have readonly fields instead of readonly properties, which will be more convenient and easier to read.
+// Unfortunately, MiniValidation lib, that used to validate this structure, doesn't support DataAnnotations attributes for fields.
+[method: JsonConstructor]
+public readonly struct MetricRecord(string name, DateTime logTimeUtc, string commitGitHubUrl, string commitMessage, string value, string units)
 {
     [Required]
     // TODO: Combine into one attribute since these are used in a few places
     [MinLength(MetricRecordConstants.MinMetricNameLength)]
     [MaxLength(MetricRecordConstants.MaxMetricNameLength)]
-    public required string Name { get; init; }
+    public string Name { get; } = name;
 
     [Required]
-    public required DateTime LogTimeUtc { get; init; }
+    public DateTime LogTimeUtc { get; } = logTimeUtc;
 
     [Required]
     [MinLength(MetricRecordConstants.MinCommitGitHubUrlLength, ErrorMessage = MetricRecordConstants.CommitGitHubUrlShorterErrorMessage)]
     [MaxLength(MetricRecordConstants.MaxCommitGitHubUrlLength)]
     [Url]
     [RegularExpression(@"https:\/\/github\.com\/[^\/]+\/[^\/]+\/commit\/[\da-fA-F]{8}", ErrorMessage = "Invalid GitHub commit URL")]
-    public required string CommitGitHubUrl { get; init; }
+    public string CommitGitHubUrl { get; } = commitGitHubUrl;
 
     [Required]
     [MinLength(MetricRecordConstants.MinStringLength)]
     [MaxLength(MetricRecordConstants.MaxCommitMessageLength)]
-    public required string CommitMessage { get; init; }
+    public string CommitMessage { get; } = commitMessage;
 
     [Required]
     [MinLength(MetricRecordConstants.MinStringLength)]
-    public required string Value { get; init; }
+    public string Value { get; } = value;
 
     [Required]
     [MinLength(MetricRecordConstants.MinStringLength)]
     [MaxLength(MetricRecordConstants.MaxUnitsLength)]
-    public required string Units { get; init; }
+    public string Units { get; } = units;
 }
