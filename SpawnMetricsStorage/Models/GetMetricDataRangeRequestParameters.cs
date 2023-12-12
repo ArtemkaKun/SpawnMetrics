5e8fc3d2a3ce292b472da3a430c8b68cc2b7f425
Artem Yurchenko <zumikq78@gmail.com>
2023-12-12 12:06:21 +0100

    Refactor: GetMetricDataRangeRequestParameters - NOTE was added

--- SpawnMetricsStorage/Models/GetMetricDataRangeRequestParameters.cs
+++ SpawnMetricsStorage/Models/GetMetricDataRangeRequestParameters.cs
@@ -6,6 +6,8 @@ using SpawnMetricsStorage.Utils;
 
 namespace SpawnMetricsStorage.Models;
 
+// NOTE: This class can be readonly structure instead of class, which will be more convenient dues to the logic.
+// Unfortunately, MiniValidation lib, that used to validate this structure, doesn't support validation of value types.
 [UsedImplicitly]
 public sealed class GetMetricDataRangeRequestParameters
 {
