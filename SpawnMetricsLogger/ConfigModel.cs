64ede3efa11cd8c1d7b2d4c48e7baf7f06f57ead
Artem Yurchenko <zumikq78@gmail.com>
2023-12-17 15:30:38 +0100

    Implement: SpawnMetricsLogger logic

---
+++ SpawnMetricsLogger/ConfigModel.cs
@@ -0,0 +1,59 @@
+using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+using MetricRecordModel;
+using SpawnMetricsStorage.Models.ProjectName;
+
+namespace SpawnMetricsLogger;
+
+// NOTE: This class can be readonly structure instead of class, which will be more convenient dues to the logic.
+// Unfortunately, MiniValidation lib, that used to validate this structure, doesn't support validation of value types.
+[method: JsonConstructor]
+public sealed class ConfigModel(string dataServerUrl, string branchName, string remoteName, string projectName, string baseCommitGitHubUrl, List<MetricOperationModel> metricOperations)
+{
+    [Required]
+    [MinLength(21)]
+    [Url]
+    public string dataServerUrl { get; } = dataServerUrl;
+
+    [Required]
+    [MinLength(3)]
+    public string branchName { get; } = branchName;
+
+    [Required]
+    [MinLength(3)]
+    public string remoteName { get; } = remoteName;
+
+    [Required]
+    [IsValidProjectName]
+    public string projectName { get; } = projectName;
+
+    [Required]
+    [MinLength(MetricRecordConstants.MinCommitGitHubUrlLength, ErrorMessage = MetricRecordConstants.CommitGitHubUrlShorterErrorMessage)]
+    [MaxLength(MetricRecordConstants.MaxCommitGitHubUrlLength)]
+    [Url]
+    [RegularExpression(@"https:\/\/github\.com\/[^\/]+\/[^\/]+\/commit\/", ErrorMessage = "Invalid base GitHub commit URL")]
+    public string baseCommitGitHubUrl { get; } = baseCommitGitHubUrl;
+
+    [Required]
+    [MinLength(1)]
+    public List<MetricOperationModel> metricOperations { get; } = metricOperations;
+}
+
+// NOTE: This structure can have readonly fields instead of readonly properties, which will be more convenient and easier to read.
+// Unfortunately, MiniValidation lib, that used to validate this structure, doesn't support DataAnnotations attributes for fields.
+[method: JsonConstructor]
+public readonly struct MetricOperationModel(string name, string command, string units)
+{
+    [Required]
+    [IsValidMetricName]
+    public string name { get; } = name;
+
+    [Required]
+    [MinLength(5)]
+    public string command { get; } = command;
+
+    [Required]
+    [MinLength(MetricRecordConstants.MinStringLength)]
+    [MaxLength(MetricRecordConstants.MaxUnitsLength)]
+    public string units { get; } = units;
+}
--- SpawnMetricsLogger/Program.cs
+++ SpawnMetricsLogger/Program.cs
@@ -1,1 +1,204 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System.CommandLine;
+using System.Diagnostics;
+using System.Text;
+using System.Text.Json;
+using LibGit2Sharp;
+using MetricRecordModel;
+using MiniValidation;
+using SharedConstants;
+using SpawnMetricsLogger;
+using SpawnMetricsStorage.Controllers;
+using SpawnMetricsStorage.Models;
+
+var repoPathOption = new Option<string>("--repo-path", description: "The path to the repository");
+var configPathOption = new Option<string>("--config-path", description: "The path to the config file");
+var adminApiKeyOption = new Option<string>("--admin-api-key", description: "The admin API key");
+var logEveryCommitOption = new Option<bool>("--log-every-commit", getDefaultValue: () => false, description: "Log every commit if set");
+
+var rootCommand = new RootCommand
+{
+    repoPathOption,
+    configPathOption,
+    adminApiKeyOption,
+    logEveryCommitOption
+};
+
+rootCommand.SetHandler((repoPath, configPath, adminApiKey, isLogEveryCommit) =>
+{
+    var config = JsonSerializer.Deserialize<ConfigModel>(File.ReadAllText(configPath), new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+
+    if (config == null)
+    {
+        Console.WriteLine("Failed to deserialize config");
+        return;
+    }
+
+    var isValid = MiniValidator.TryValidate(config, out var errors);
+
+    if (!isValid)
+    {
+        Console.WriteLine("Config is invalid:");
+
+        foreach (var error in errors)
+        {
+            Console.WriteLine(error.Key);
+
+            foreach (var errorMessage in error.Value)
+            {
+                Console.WriteLine(errorMessage);
+            }
+        }
+
+        return;
+    }
+
+    var httpClient = new HttpClient();
+    httpClient.BaseAddress = new Uri(config.dataServerUrl);
+    httpClient.DefaultRequestHeaders.Add(MetricsControllerConstants.ApiKeyParameter, adminApiKey);
+
+    if (isLogEveryCommit)
+    {
+        using var repo = new Repository(repoPath);
+
+        Commands.Checkout(repo, config.branchName);
+
+        var remote = repo.Network.Remotes[config.remoteName];
+        var refSpecs = remote.FetchRefSpecs.Select(x => x.Specification);
+        Commands.Fetch(repo, config.remoteName, refSpecs, null, "Ref was updated");
+
+        foreach (var commit in repo.Branches[config.branchName].Commits)
+        {
+            Commands.Checkout(repo, commit, new CheckoutOptions { CheckoutModifiers = CheckoutModifiers.Force, CheckoutNotifyFlags = CheckoutNotifyFlags.None });
+
+            var commitLogTimeUtc = commit.Committer.When.UtcDateTime;
+            var commitGitHubUrl = $"{config.baseCommitGitHubUrl}{commit.Sha[..MetricRecordConstants.ShortCommitHashLength]}";
+
+            foreach (var metricOperation in config.metricOperations)
+            {
+                var process = Process.Start(new ProcessStartInfo
+                {
+                    FileName = "bash",
+                    WorkingDirectory = repoPath,
+                    Arguments = $"-c \"{metricOperation.command}\"",
+                    RedirectStandardOutput = true,
+                    UseShellExecute = false,
+                    CreateNoWindow = true
+                });
+
+                if (process == null)
+                {
+                    Console.WriteLine($"Failed to start process for metric {metricOperation.name}");
+                    return;
+                }
+
+                process.WaitForExit();
+
+                if (process.ExitCode != 0)
+                {
+                    Console.WriteLine($"Failed to get metric {metricOperation.name}");
+                    return;
+                }
+
+                var output = process.StandardOutput.ReadToEnd();
+
+                if (string.IsNullOrWhiteSpace(output))
+                {
+                    Console.WriteLine($"Failed to get metric {metricOperation.name}");
+                    return;
+                }
+
+                var metric = new MetricRecord(metricOperation.name, commitLogTimeUtc, commitGitHubUrl, commit.Message.Trim(), output.Trim(), metricOperation.units);
+
+                var requestBody = new LogMetricRequestBody
+                {
+                    ProjectName = config.projectName,
+                    Metric = metric
+                };
+
+                var requestBodyJson = JsonSerializer.Serialize(requestBody);
+
+                var response = httpClient.PutAsync(EndpointsConstants.MetricEndpoint, new StringContent(requestBodyJson, Encoding.UTF8, "application/json")).Result;
+
+                if (response.IsSuccessStatusCode == false)
+                {
+                    Console.WriteLine($"Failed to log metric {metricOperation.name}");
+                }
+                else
+                {
+                    Console.WriteLine($"Logged metric {commit.Sha}");
+                }
+            }
+        }
+
+        Commands.Checkout(repo, config.branchName);
+    }
+    else
+    {
+        using var repo = new Repository(repoPath);
+
+        Commands.Checkout(repo, config.branchName);
+
+        var currentCommit = repo.Head.Tip;
+
+        var commitLogTimeUtc = currentCommit.Committer.When.UtcDateTime;
+        var commitGitHubUrl = $"{config.baseCommitGitHubUrl}{currentCommit.Sha[..MetricRecordConstants.ShortCommitHashLength]}";
+
+        foreach (var metricOperation in config.metricOperations)
+        {
+            var process = Process.Start(new ProcessStartInfo
+            {
+                FileName = "bash",
+                WorkingDirectory = repoPath,
+                Arguments = $"-c \"{metricOperation.command}\"",
+                RedirectStandardOutput = true,
+                UseShellExecute = false,
+                CreateNoWindow = true
+            });
+
+            if (process == null)
+            {
+                Console.WriteLine($"Failed to start process for metric {metricOperation.name}");
+                return;
+            }
+
+            process.WaitForExit();
+
+            if (process.ExitCode != 0)
+            {
+                Console.WriteLine($"Failed to get metric {metricOperation.name}");
+                return;
+            }
+
+            var output = process.StandardOutput.ReadToEnd();
+
+            if (string.IsNullOrWhiteSpace(output))
+            {
+                Console.WriteLine($"Failed to get metric {metricOperation.name}");
+                return;
+            }
+
+            var metric = new MetricRecord(metricOperation.name, commitLogTimeUtc, commitGitHubUrl, currentCommit.Message.Trim(), output.Trim(), metricOperation.units);
+
+            var requestBody = new LogMetricRequestBody
+            {
+                ProjectName = config.projectName,
+                Metric = metric
+            };
+
+            var requestBodyJson = JsonSerializer.Serialize(requestBody);
+
+            var response = httpClient.PutAsync(EndpointsConstants.MetricEndpoint, new StringContent(requestBodyJson, Encoding.UTF8, "application/json")).Result;
+
+            if (response.IsSuccessStatusCode == false)
+            {
+                Console.WriteLine($"Failed to log metric {metricOperation.name}");
+            }
+            else
+            {
+                Console.WriteLine($"Logged metric {currentCommit.Sha}");
+            }
+        }
+    }
+}, repoPathOption, configPathOption, adminApiKeyOption, logEveryCommitOption);
+
+return rootCommand.Invoke(args);
