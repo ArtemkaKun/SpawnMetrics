7ddcbfef9f44128ea8c3c5d3e288daf860a8faec
Artem Yurchenko <zumikq78@gmail.com>
2023-12-18 16:31:20 +0100

    Fix: Add CORS policy for Storage in production

--- SpawnMetricsStorage/Program.cs
+++ SpawnMetricsStorage/Program.cs
@@ -4,10 +4,20 @@ using SpawnMetricsStorage.Utils.SurrealDb;
 
 var builder = WebApplication.CreateBuilder(args);
 
+if (builder.Environment.IsProduction())
+{
+    builder.Services.AddCors(options => { options.AddPolicy("CorsPolicy", policyBuilder => { policyBuilder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader(); }); });
+}
+
 var app = builder.Build();
 
 var surrealDbClient = SurrealDbCreator.CreateSurrealDbClient(builder.Configuration);
 var metricsService = new MetricsService(surrealDbClient);
 new MetricsController(metricsService, builder.Configuration).RegisterEndpoints(app);
+
+if (builder.Environment.IsProduction())
+{
+    app.UseCors("CorsPolicy");
+}
 
 app.Run();
